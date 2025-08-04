db3ccc969e1321bfc4d436314d6baf574c6eea34
duymanhdoan <doanmanhduy.yb0210@gmail.com>
2025-08-04 17:42:36 +0700

    update pytest file

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,47 +1,30 @@
-import re
-from pathlib import Path
-
 import pytest
 
 
-def find_cases():
-    root_dir = Path(__file__).parent / 'cases_update'
-    for f in root_dir.iterdir():
-        if not f.is_file():
-            continue
-        if f.suffix in {'.py', '.md'}:
-            content = f.read_text()
-            m = re.search(r'(.+?)^#{5,}', content, flags=re.M | re.I | re.S)
-            assert m, f'No EXAMPLE match found for {f}'
-            example = m.group(1).strip('\n')
-            m = re.search(r'^#{5,}\s*output\s*#{5,}\n(.+?)^#{5,}', content, flags=re.M | re.I | re.S)
-            assert m, f'No OUTPUT match found for {f}'
-            output = m.group(1).strip('\n')
-            m = re.search(r'^#{5,}\s*test\s*#{5,}\n(.+)', content, flags=re.M | re.I | re.S)
-            assert m, f'No TEST match found for {f}'
-            test = m.group(1).strip('\n')
-            m_test_count = re.search(r'^test_count *= *(\d+)', test, flags=re.M)
-            if m_test_count:
-                test_count = int(m_test_count.group(1))
-            else:
-                test_count = 1
-
-            if f.suffix == '.md':
-                m = re.search(r'^```.*?^(.+?)^```', test, flags=re.M | re.S)
-                if m:
-                    test = m.group(1)
-            yield pytest.param(f, example, output, test, test_count, id=f.name)
+def test_div_zero_exception():
+    """
+    pytest.raises can assert that exceptions are raised (catching them)
+    """
+    with pytest.raises(ZeroDivisionError):
+        x = 1 / 0
 
 
-@pytest.mark.parametrize('file_path,example,output,test_code,test_count', find_cases())
-def test_cases_update(
-    pytester: pytest.Pytester, file_path: Path, example: str, output: str, test_code: str, test_count: int
-):
-    input_file = pytester.makefile(file_path.suffix, **{f'case_{file_path.stem}': example})
-    pytester.makepyfile(test_code)
-    result = pytester.runpytest('-p', 'no:pretty', '-vs', '--update-examples', '--update-examples-disable-summary')
-    result.assert_outcomes(passed=test_count)
+def test_keyerror_details():
+    """
+    The raised exception can be referenced, and further inspected (or asserted)
+    """
+    my_map = {"foo": "bar"}
 
-    # debug(input_file.read_text(), output)
-    # print(input_file.read_text())
-    assert input_file.read_text() == output
+    with pytest.raises(KeyError) as ke:
+        baz = my_map["baz"]
+
+    # Our KeyError should reference the missing key, "baz"
+    assert "baz" in str(ke)
+
+
+def test_approximate_matches():
+    """
+    pytest.approx can be used to assert "approximate" numerical equality
+    (compare to "assertAlmostEqual" in unittest.TestCase)
+    """
+    assert 0.1 + 0.2 == pytest.approx(0.3)
